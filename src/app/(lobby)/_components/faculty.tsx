68e8bc7ebe70bbf7abd26163081dac4cd5820511
unknown <duyka6203@gmail.com>
2024-03-19 04:37:19 +0700

    feat: add feature faculty and recent-blog

--- src/app/(lobby)/_components/faculty.tsx
+++ src/app/(lobby)/_components/faculty.tsx
@@ -1,19 +1,119 @@
-import * as React from 'react';
+import * as React from "react"
 
 import styles from "@/styles/(lobby)/_components/faculty.module.scss"
 
 export function Faculty() {
   return (
     <div className={styles["faculty-wrapper"]}>
-      <div className={styles['faculty-container']}>
+      <div className={styles["faculty-container"]}>
         <div className={styles["faculty-header-wrapper"]}>
           <div className={styles["faculty-header-container"]}>
             <h2>Our Faculties</h2>
-            <p>We help students achieve academic excellence in a diverse range of disciplines and fields – through our excellent faculties.</p>
+            <p>
+              We help students achieve academic excellence in a diverse range of
+              disciplines and fields – through our excellent faculties.
+            </p>
           </div>
         </div>
-        <div></div>
+        <div className={styles["faculty-card-wrapper"]}>
+          <div className={styles["faculty-card"]}>
+            <div
+              className={styles["faculty-card-img"]}
+              style={{ backgroundImage: "url(images/course-1.jpg)" }}
+            ></div>
+            <div className={styles["faculty-card-content"]}>
+              <p className={styles["faculty-card-info"]}>
+                <span>
+                  <i className="icon-user mr-2"></i>Mr. Khan
+                </span>
+                <span>
+                  <i className="icon-table mr-2"></i>10 seats
+                </span>
+                <span>
+                  <i className="icon-calendar mr-2"></i>4 Years
+                </span>
+              </p>
+              <h3>
+                <a href="#">Electric Engineering</a>
+              </h3>
+              <p>
+                Separated they live in. A small river named Duden flows by their
+                place and supplies it with the necessary regelialia. It is a
+                paradisematic country
+              </p>
+              <p>
+                <a href="#" className={styles["faculty-card-submit-btn"]}>
+                  Apply now
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className={styles["faculty-card"]}>
+            <div
+              className={styles["faculty-card-img"]}
+              style={{ backgroundImage: "url(images/course-2.jpg)" }}
+            ></div>
+            <div className={styles["faculty-card-content"]}>
+              <p className={styles["faculty-card-info"]}>
+                <span>
+                  <i className="icon-user mr-2"></i>Mr. Khan
+                </span>
+                <span>
+                  <i className="icon-table mr-2"></i>10 seats
+                </span>
+                <span>
+                  <i className="icon-calendar mr-2"></i>4 Years
+                </span>
+              </p>
+              <h3>
+                <a href="#">Electric Engineering</a>
+              </h3>
+              <p>
+                Separated they live in. A small river named Duden flows by their
+                place and supplies it with the necessary regelialia. It is a
+                paradisematic country
+              </p>
+              <p>
+                <a href="#" className={styles["faculty-card-submit-btn"]}>
+                  Apply now
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className={styles["faculty-card"]}>
+            <div
+              className={styles["faculty-card-img"]}
+              style={{ backgroundImage: "url(images/course-3.jpg)" }}
+            ></div>
+            <div className={styles["faculty-card-content"]}>
+              <p className={styles["faculty-card-info"]}>
+                <span>
+                  <i className="icon-user mr-2"></i>Mr. Khan
+                </span>
+                <span>
+                  <i className="icon-table mr-2"></i>10 seats
+                </span>
+                <span>
+                  <i className="icon-calendar mr-2"></i>4 Years
+                </span>
+              </p>
+              <h3>
+                <a href="#">Electric Engineering</a>
+              </h3>
+              <p>
+                Separated they live in. A small river named Duden flows by their
+                place and supplies it with the necessary regelialia. It is a
+                paradisematic country
+              </p>
+              <p>
+                <a href="#" className={styles["faculty-card-submit-btn"]}>
+                  Apply now
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  );
+  )
 }
--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -4,6 +4,7 @@ import { Shell } from "@/components/shells/shell"
 import styles from "@/styles/(lobby)/page.module.scss"
 import { CarouselSize } from "@/app/(lobby)/_components/carousel"
 import { Faculty } from "@/app/(lobby)/_components/faculty"
+import { RecentBlog } from "@/app/(lobby)/_components/recent-blog"
 
 // const faculties = [
 //   { faculty: "IT & Software", description: "100 Workspace", href: "/" },
@@ -16,6 +17,7 @@ export default function Page() {
     <Shell as={"div"} className={styles["shell"]}>
       <CarouselSize />
       <Faculty />
+      <RecentBlog />
     </Shell>
   )
 }
