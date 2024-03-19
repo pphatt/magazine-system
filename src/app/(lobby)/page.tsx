e258643609ec093a5cd400f2315de06d23fb7833
pphatt <vutienphat0809@gmail.com>
2024-03-19 12:20:09 +0700

    refactor(lobby): refactor faculty secion

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -6,12 +6,6 @@ import { CarouselSize } from "@/app/(lobby)/_components/carousel"
 import { Faculty } from "@/app/(lobby)/_components/faculty"
 import { RecentBlog } from "@/app/(lobby)/_components/recent-blog"
 
-// const faculties = [
-//   { faculty: "IT & Software", description: "100 Workspace", href: "/" },
-//   { faculty: "Photography", description: "100 Workspace", href: '/' },
-//   { faculty: "Marketing", description: "100 Workspace", href: "/" },
-// ]
-
 export default function Page() {
   return (
     <Shell as={"div"} className={styles["shell"]}>
