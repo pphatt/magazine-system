06905d3390a6b404220fcb42f417749366ea6927
phatshibukai <vutienphat0809@gmail.com>
2024-03-20 13:14:55 +0700

    fix: fix linting

--- src/app/(lobby)/_components/recent-blog.tsx
+++ src/app/(lobby)/_components/recent-blog.tsx
@@ -2,7 +2,6 @@ import * as React from "react"
 
 import { Button } from "@/components/ui/button"
 import styles from "@/styles/(lobby)/_components/recent-blog.module.scss"
-import {Button} from "@/components/ui/button";
 
 export function RecentBlog() {
   return (
