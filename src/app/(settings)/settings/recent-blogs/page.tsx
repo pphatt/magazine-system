7d4c2ab265d40403e144b6519f89ed16364d6a9c
BruyneHuu <buinghialk180303@gmail.com>
2024-04-20 17:03:17 +0700

    feat(recent-blogs): show blog like

--- src/app/(settings)/settings/recent-blogs/page.tsx
+++ src/app/(settings)/settings/recent-blogs/page.tsx
@@ -14,6 +14,7 @@ import { recentBlogParamsSchema } from "@/lib/validations/params"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
+import { LikeBtn } from "@/components/like-btn"
 import { PaginationRecentBlog } from "@/components/pagination/pagination-recent-blog"
 import styles from "@/styles/(settings)/recent-blogs/page.module.scss"
 import { SelectAcademicYearInput } from "@/app/(lobby)/contribution/_components/select-academic-year"
@@ -103,70 +104,97 @@ export default async function RecentBlogsPage({
               status,
               comments,
               marketingCoordinator,
+              like,
             },
             index
-          ) => (
-            <article className={styles["article-wrapper"]} key={index}>
-              <div className={styles["article-container"]}>
-                <div className={styles["article-header-wrapper"]}>
-                  <div className={styles["article-header-container"]}>
-                    <div className={styles["author-avatar-wrapper"]}>
-                      <Avatar className={styles["avatar"]}>
-                        <AvatarImage
-                          src={author.image ?? ""}
-                          alt={""}
-                          style={{
-                            objectFit: "cover",
-                            objectPosition: "top",
-                          }}
-                        />
-                        <AvatarFallback>
-                          <Icons.user />
-                        </AvatarFallback>
-                      </Avatar>
+          ) => {
+            const commentsCount = comments.filter(
+              (comment) => !comment.replyToId
+            ).length
+
+            const initialLike = like.some(
+              ({ userId, blogId }) => userId === user.id && blogId === id
+            )
+
+            return (
+              <article className={styles["article-wrapper"]} key={index}>
+                <div className={styles["article-container"]}>
+                  <div className={styles["article-header-wrapper"]}>
+                    <div className={styles["article-header-container"]}>
+                      <div className={styles["author-avatar-wrapper"]}>
+                        <Avatar className={styles["avatar"]}>
+                          <AvatarImage
+                            src={author.image ?? ""}
+                            alt={""}
+                            style={{
+                              objectFit: "cover",
+                              objectPosition: "top",
+                            }}
+                          />
+                          <AvatarFallback>
+                            <Icons.user />
+                          </AvatarFallback>
+                        </Avatar>
+                      </div>
+                      <div>
+                        <div className={styles["author-name"]}>
+                          {author.name}
+                        </div>
+                        <div>Created at: {format(createdAt, "PPP")}</div>
+                        <div>Updated at: {format(updatedAt, "PPP")}</div>
+                      </div>
                     </div>
-                    <div>
-                      <div className={styles["author-name"]}>{author.name}</div>
-                      <div>Created at: {format(createdAt, "PPP")}</div>
-                      <div>Updated at: {format(updatedAt, "PPP")}</div>
+                    <div className={styles["status-wrapper"]}>
+                      <div
+                        className={styles["upload-status"]}
+                        data-submit={status.toLowerCase()}
+                      >
+                        {status.toLowerCase()}
+                      </div>
                     </div>
                   </div>
-                  <div className={styles["status-wrapper"]}>
-                    <div
-                      className={styles["upload-status"]}
-                      data-submit={status.toLowerCase()}
-                    >
-                      {status.toLowerCase()}
+                  <div className={styles["article-content-wrapper"]}>
+                    <h3 className={styles["article-title"]}>
+                      <Link
+                        href={`/contribution/blog/${id}`}
+                        className={styles["article-link"]}
+                      >
+                        {title}
+                      </Link>
+                    </h3>
+                    <p className={styles["article-description"]}>
+                      {marketingCoordinator?.name
+                        ? `Graded by: ${marketingCoordinator?.name}`
+                        : "Not graded yet"}
+                    </p>
+                    <div className={styles["article-comments-wrapper"]}>
+                      <LikeBtn
+                        blogId={id}
+                        likeCount={like.length}
+                        initialLike={initialLike}
+                        className={styles["like-btn"]}
+                      >
+                        <span>
+                          {like.length} {like.length > 1 ? "likes" : "like"}
+                        </span>
+                      </LikeBtn>
+
+                      <Button
+                        variant={"ghost"}
+                        className={styles["comment-btn"]}
+                      >
+                        <Icons.messageCircle />
+                        <span>
+                          {commentsCount}{" "}
+                          {commentsCount > 1 ? "comments" : "comment"}
+                        </span>
+                      </Button>
                     </div>
                   </div>
                 </div>
-                <div className={styles["article-content-wrapper"]}>
-                  <h3 className={styles["article-title"]}>
-                    <Link
-                      href={`/contribution/blog/${id}`}
-                      className={styles["article-link"]}
-                    >
-                      {title}
-                    </Link>
-                  </h3>
-                  <p className={styles["article-description"]}>
-                    {marketingCoordinator?.name
-                      ? `Graded by: ${marketingCoordinator?.name}`
-                      : "Not graded yet"}
-                  </p>
-                  <div className={styles["article-comments-wrapper"]}>
-                    <Button variant={"ghost"} className={styles["comment-btn"]}>
-                      <Icons.messageCircle />
-                      {
-                        comments.filter((comment) => !comment.replyToId).length
-                      }{" "}
-                      comments
-                    </Button>
-                  </div>
-                </div>
-              </div>
-            </article>
-          )
+              </article>
+            )
+          }
         )}
       </div>
 
